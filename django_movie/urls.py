16ebe85d2dc17483b55720479aea1830c90a498a
tturdumamatovv <muki2022@mail.ru>
2022-09-12 18:37:53 +0600

    .

---
+++ django_movie/urls.py
@@ -0,0 +1,23 @@
+"""django_movie URL Configuration
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from .yasg import urlpatterns as doc_url
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+    path('api/v1/', include('movies.urls')),
+]
+
+urlpatterns += doc_url
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
